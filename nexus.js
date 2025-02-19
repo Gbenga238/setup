5b8c3404018ba6046c28daf738c7041bc6be4203
gbenga238 <aanuoluwapo238@gmail.com>
2025-02-19 07:19:27 +0100

    update

--- nexus.js
+++ nexus.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2025-02-19
 // @description  Keeps the power button ON
-// @author       You
+// @author       Gbenga Olajide
 // @match        https://app.nexus.xyz/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nexus.xyz
 // @grant        none
